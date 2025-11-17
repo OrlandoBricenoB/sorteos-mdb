b5d143efe3dfd1176642db508c446503d1b2849c
orlandobricenob <orlando.briceno.blanco@gmail.com>
2025-11-17 10:26:09 -0400

    build: Change astro page to static one

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: cloudflare(),
-  output: 'server',
-  vite: {
-    optimizeDeps: {
-      exclude: ['@astro-page']
-    }
-  }
+  output: 'static',
 });
