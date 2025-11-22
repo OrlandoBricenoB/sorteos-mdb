a7b84cb12b808fd113e4c81bbb9fb031272dd6e1
orlandobricenob <orlando.briceno.blanco@gmail.com>
2025-11-22 12:26:59 -0400

    feat: Deploy SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Habilitar SSR
+  adapter: cloudflare({
+    mode: 'directory', // Modo de deployment para Cloudflare Pages
+  }),
   integrations: [tailwind()],
 });
